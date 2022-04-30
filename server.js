736009f4912591d216782696500cfb5994dfa651
ortheog17@gmail.com <ortheog17@gmail.com>
2022-04-30 21:02:44 +0300

    try-fix-cors

--- server.js
+++ server.js
@@ -18,7 +18,15 @@ app.use((req,res,next)=>{
     if(allowedOrigins.includes(origin)){
         res.setHeader('Access-Control-Allow-Origin',origin);
     }
-})
+    next();
+});
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});;
 
 app.use(cors({
     origin:'https://nodejs-songs-website.herokuapp.com/'
